97d5b464e92c51d3cd7a73fdc0f77db56e5633f6
Usamah <usamah13@students.cs.ubc.ca>
2021-11-26 18:52:51 -0800

    fixed bugs check it out

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,8 +8,6 @@ import { Output } from './mocks/output';
 function App() {
   const [text, setText] = React.useState("");
   const [output, setOutput] = React.useState<Output[]>([]);
-  console.log(output.length + " output array");
-  console.log(output );
   if (!output.length) {
     return <InputView text={text} setText={setText} setOutput={setOutput} />
   } else {
