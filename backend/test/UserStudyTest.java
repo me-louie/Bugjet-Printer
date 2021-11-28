dc0710d0ea8e51cb3e470772c888a55284e36bba
melouie <me-louie@users.noreply.github.com>
2021-11-28 12:27:08 -0800

    simplify UserStudyTest

--- backend/test/UserStudyTest.java
+++ backend/test/UserStudyTest.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class UserStudyTest {
 
     public static void main(String[] args) {
@@ -8,7 +10,7 @@ public class UserStudyTest {
     // arr1 = {10, 11, 12, 13, 14};
     // arr2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    // Goal: swap array values such that the end states look like the following:
+    // Goal: swap array values such that the end states are the following:
     // arr1 = {10, 9, 8, 7, 6}
     // arr2 = {1, 2, 3, 4, 5, 14, 13, 12, 11, 10}
 
@@ -21,17 +23,8 @@ public class UserStudyTest {
             arr1[i] = arr2[j];
             arr2[j] = temp;
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append("arr1: \n");
-        for (int a = 0; a < arr1.length; a++) {
-            sb.append(arr1[a] + ", ");
-        }
-        sb.append("\n arr2: \n");
-
-        for (int b = 0; b < arr2.length; b++) {
-            sb.append(arr2[b] + ", ");
-        }
-        System.out.println(sb);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
     }
 }
 
